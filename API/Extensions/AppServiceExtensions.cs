d9fca0aa02bd741b469dc69fd07026b99db411ac
Title <mr.yossanan035@gmail.com>
2023-12-20 09:09:11 +0700

    Implement new feature XYZ

---
+++ API/Extensions/AppServiceExtensions.cs
@@ -0,0 +1,30 @@
+﻿using API.Data;
+using API.Interfaces;
+using API.Middleware;
+using Company.ClassLibrary1;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace API.Extensions;
+
+public static class AppServiceExtensions
+{
+    public static IServiceCollection AddAppServices(this IServiceCollection services, IConfiguration conf)
+    {
+        services.AddDbContext<DataContext>(opt =>
+            {
+                opt.UseSqlite(conf.GetConnectionString("DefaultConnection"));
+            });
+
+        services.AddCors();
+
+        services.AddScoped<ITokenService, TokenService>();
+        // services.AddSingleton<ExceptionMiddleware>();
+
+
+
+
+        return services;
+    }
+
+}
---
+++ client/src/app/nav/nav.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core'
+import { AccountService } from '../_services/account.service'
+import { Observable, of } from 'rxjs'
+import { User } from '../_models/user'
+import { Router, Routes } from '@angular/router'
+import { ToastrService } from 'ngx-toastr'
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+  model: { username: string | undefined, password: string | undefined } = {
+    username: undefined,
+    password: undefined
+  }
+
+  currentUser$: Observable<User | null> = of(null)
+
+  constructor(private accountService: AccountService, private router: Router, private toastr: ToastrService) { }
+
+  ngOnInit(): void {
+    this.currentUser$ = this.accountService.currentUser$
+  }
+
+  getCurrentUser() {
+    this.accountService.currentUser$.subscribe({
+      // next: user => this.isLogin = !!user, // user?true:false
+      error: err => console.log(err)
+    })
+  }
+
+  login(): void {
+    this.accountService.login(this.model).subscribe({ //Observable
+      next: response => {
+        this.router.navigateByUrl('/members')
+      },
+      // error: err => this.toastr.error(err.error)
+    })
+  }
+
+  logout() {
+    this.accountService.logout()
+    this.router.navigateByUrl("/")
+  }
+}
